1c225f50b221ca6fb91f2a1b31950a21df1abf4e
Vladislav Yundin <4yundin@gmail.com>
2022-05-22 13:50:52 +0400

    build: introduce version catalog and shared sdk versions

--- sources/base/app/src/main/java/com/yundin/tangodemo/MainActivity.kt
+++ sources/base/app/src/main/java/com/yundin/tangodemo/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.yundin.tangodemo.ui.theme.TangoDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,12 +31,4 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TangoDemoTheme {
-        Greeting("Android")
-    }
 }
